e5549e9f7248f8188f4b7526a1d60b8c622d3693
김재욱 <soo8848@naver.com>
2025-12-26 14:42:47 +0900

    first commit

---
+++ src/main/java/org/zero/plantoryapigatewayservice/PlantoryApigatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package org.zero.plantoryapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantoryApigatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantoryApigatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zero/plantoryapigatewayservice/PlantoryApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zero.plantoryapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantoryApigatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
