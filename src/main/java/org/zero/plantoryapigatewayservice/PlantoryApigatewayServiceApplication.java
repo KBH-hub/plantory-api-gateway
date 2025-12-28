fff583371ca89901cb67a442cbca6ad6313cde5d
soo8848 <soo8848@naver.com>
2025-12-28 17:56:47 +0900

    fix

--- src/main/java/org/zero/plantoryapigatewayservice/PlantoryApigatewayServiceApplication.java
+++ src/main/java/org/zero/plantoryapigatewayservice/PlantoryApigatewayServiceApplication.java
@@ -2,8 +2,10 @@ package org.zero.plantoryapigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "org.zero.plantoryapigatewayservice")
 public class PlantoryApigatewayServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/zero/plantoryapigatewayservice/security/SecurityProperties.java
+++ src/main/java/org/zero/plantoryapigatewayservice/security/SecurityProperties.java
@@ -1,14 +1,22 @@
 package org.zero.plantoryapigatewayservice.security;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 @ConfigurationProperties(prefix = "security")
 public class SecurityProperties {
+
     private List<String> whitelist = new ArrayList<>();
 
-    public List<String> getWhitelist() { return whitelist; }
-    public void setWhitelist(List<String> whitelist) { this.whitelist = whitelist; }
+    public List<String> getWhitelist() {
+        return whitelist;
+    }
+
+    public void setWhitelist(List<String> whitelist) {
+        this.whitelist = whitelist;
+    }
 }
